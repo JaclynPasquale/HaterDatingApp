13f49bbec996867d0cfe8242bb26bf035dc14946
JaclynPasquale <jaclynpasquale@gmail.com>
2015-03-17 15:02:33 -0600

    added UserModel and AuthContext

--- HaterDatingApp/Startup.cs
+++ HaterDatingApp/Startup.cs
@@ -1,14 +1,22 @@
 ﻿using Microsoft.Owin;
 using Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
 
-[assembly: OwinStartupAttribute(typeof(HaterDatingApp.Startup))]
-namespace HaterDatingApp
+[assembly: OwinStartup(typeof(HaterDatingApp.API.Startup))]
+namespace HaterDatingApp.API
 {
-    public partial class Startup
+    public class Startup
     {
         public void Configuration(IAppBuilder app)
         {
-            ConfigureAuth(app);
+            HttpConfiguration config = new HttpConfiguration();
+            WebApiConfig.Register(config);
+            app.UseWebApi(config);
         }
+
     }
 }
