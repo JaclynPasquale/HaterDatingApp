9e50da88cebda9177e7421c456d2ac718927554b
JaclynPasquale <jaclynpasquale@gmail.com>
2015-03-17 13:44:26 -0600

    Added context

---
+++ HaterDatingApp/HaterDatingContext.cs
@@ -0,0 +1,18 @@
+﻿using HaterDatingApp.Model;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace HaterDatingApp
+{
+    
+        public class HaterDatingContext : DbContext
+        {
+            public DbSet<Profile> Profiles { get; set; }
+            public DbSet<Dislike> Dislikes { get; set; }
+            public DbSet<Question> Questions { get; set; }
+            
+        }
+}
