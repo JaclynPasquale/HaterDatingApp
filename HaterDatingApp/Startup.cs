0c8685393f26daa0df4d8b399abb090c7dbe878f
JaclynPasquale <jaclynpasquale@gmail.com>
2015-03-17 15:44:50 -0600

    added AccountController and Providers

--- HaterDatingApp/Startup.cs
+++ HaterDatingApp/Startup.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Owin;
+using Microsoft.Owin.Security.OAuth;
 using Owin;
 using System;
 using System.Collections.Generic;
@@ -6,17 +7,33 @@ using System.Linq;
 using System.Web;
 using System.Web.Http;
 
+
 [assembly: OwinStartup(typeof(HaterDatingApp.API.Startup))]
 namespace HaterDatingApp.API
 {
-    public class Startup
+    
+public class Startup
     {
         public void Configuration(IAppBuilder app)
         {
-            HttpConfiguration config = new HttpConfiguration();
-            WebApiConfig.Register(config);
-            app.UseWebApi(config);
+            ConfigureOAuth(app);
+	    //Rest of code is here;
         }
-
+ 
+        public void ConfigureOAuth(IAppBuilder app)
+        {
+            OAuthAuthorizationServerOptions OAuthServerOptions = new OAuthAuthorizationServerOptions()
+            {
+                AllowInsecureHttp = true,
+                TokenEndpointPath = new PathString("/token"),
+                AccessTokenExpireTimeSpan = TimeSpan.FromDays(1),
+                Provider = new SimpleAuthorizationServerProvider()
+            };
+ 
+            // Token Generation
+            app.UseOAuthAuthorizationServer(OAuthServerOptions);
+            app.UseOAuthBearerAuthentication(new OAuthBearerAuthenticationOptions());
+ 
+        }
     }
 }
